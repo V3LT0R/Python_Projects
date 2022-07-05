e7d561b36f6470328355625b5a4644a2f8f549d6
V3LT0R <108691866+V3LT0R@users.noreply.github.com>
2022-07-05 18:16:43 +0200

    Add files via upload

---
+++ Python_Pass_Gen.py
@@ -0,0 +1,30 @@
+import random
+import string
+
+
+def get_random_password():
+    random_source = string.ascii_letters + string.digits + string.punctuation
+    # select 1 lowercase
+    password = random.choice(string.ascii_lowercase)
+    # select 1 uppercase
+    password += random.choice(string.ascii_uppercase)
+    # select 1 digit
+    password += random.choice(string.digits)
+    # select 1 special symbol
+    password += random.choice(string.punctuation)
+
+    # generate other characters, Change the range for a longer range.
+    for i in range(10):
+        password += random.choice(random_source)
+
+    password_list = list(password)
+    # shuffle all characters
+    random.SystemRandom().shuffle(password_list)
+    password = ''.join(password_list)
+    return password
+
+
+print("First Random Password is ", get_random_password())
+# output  qX49}]Ru!(
+print("Second Random Password is ", get_random_password())
+# Output  3nI0.V#[T
